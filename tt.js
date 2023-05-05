697654ecc12c4ed9b8a38ad36bf2dcf457036ce3
mend18 <79925769+mend18@users.noreply.github.com>
2023-05-05 12:23:57 +0100

    Add files via upload

---
+++ tt.js
@@ -0,0 +1,14 @@
+module.exports = async function (context, req) {
+ context.log('Alguém invocou a função remotamente');
+ const body = req.body;
+ const responseMessage = body
+ ? "Foram recebidos estes dados: " + JSON.stringify(body)
+ : "Por favor, envie algo no body da mensagem HTTP";
+ if (body)
+ context.log('Dados recebidos: ' + JSON.stringify(body));
+
+ context.res = {
+ // status: 200, /* Defaults to 200 */
+ body: responseMessage
+ };
+}
