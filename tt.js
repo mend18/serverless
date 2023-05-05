5c16338a2490a5565fcf34accc7099bc4f0f9708
mend18 <79925769+mend18@users.noreply.github.com>
2023-05-05 12:44:29 +0100

    Update tt.js

--- tt.js
+++ tt.js
@@ -5,7 +5,7 @@ module.exports = async function (context, req) {
  ? "Foram recebidos estes dados: " + JSON.stringify(body)
  : "Por favor, envie algo no body da mensagem HTTP";
  if (body)
- context.log('Dados recebidos: ' + JSON.stringify(body));
+ context.log('Dados recebidos yh meu wtf: ' + JSON.stringify(body));
 
  context.res = {
  // status: 200, /* Defaults to 200 */
